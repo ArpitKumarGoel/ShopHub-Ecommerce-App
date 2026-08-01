fb24859c9b7ff4bc87a0329a4ef5f84f30472d61
ArpitKumarGoel <goelarpit286@gmail.com>
2026-08-01 11:21:44 +0530

    Fix routing and components for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router'
-//import
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
 import Cart from './pages/Cart'
-import Product_Detail from "./pages/Product_Detail";
+import Product_Detail from "./pages/Product_Detail"
 import All_Products from './components/All_Products'
-import ProductByCategory  from './pages/ProductByCategory'
+import ProductByCategory from './pages/ProductByCategory'
 import Search_Product from './pages/Search_Product'
-import TrendingSlider from './components/Trending_Slider';
-import DataState from './context/DataState';
+import DataState from './context/DataState'
 
 const App = () => {
   return (
     <DataState>
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={
-              <>
-              <All_Products />
-                <TrendingSlider />
-              </>
-            } />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/product/:id" element={<Product_Detail/>} />
-        <Route path="/product/category/:cat" element={<ProductByCategory/>} />
-        <Route path="/product/search/:term" element={<Search_Product/>} />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<All_Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:id" element={<Product_Detail />} />
+          <Route path="/product/category/:cat" element={<ProductByCategory />} />
+          <Route path="/product/search/:term" element={<Search_Product />} />
+        </Routes>
+      </Router>
     </DataState>
   )
 }
